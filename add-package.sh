523f63d94465a272aed8d8c2f9b97b8210934ee0
Luiz Carvalho <lucarval@redhat.com>
2025-06-06 15:28:52 -0400

    Add helper to add package workflow
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

---
+++ add-package.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Add a package to the calunga catalog.
+#
+# Usage:
+#   add-package.sh <package-name>
+#
+
+set -euo pipefail
+
+PKG_NAME=$1
+
+cd "$(git root)"
+
+git checkout main
+git pull
+
+# We want this to fail if the directory already exists which implies the package is either already
+# onboarded or something went wrong on a previous run.
+mkdir "packages/${PKG_NAME}"
+
+./generate.sh
+
+echo
+echo "==================================================="
+echo "Package dependencies ${PKG_NAME} has been resolved."
+echo "==================================================="
+
+echo 'Creating Konflux resources for package'
+kustomize build "konflux/${PKG_NAME}" | oc -n calunga-tenant apply -f -
+
+echo 'Creating a pull request for onboarding the package'
+
+BRANCH_NAME="add-${PKG_NAME}"
+git checkout -b "${BRANCH_NAME}"
+git add .
+git commit -m "Onboard ${name} package" --signoff
+git push --set-upstream origin "${BRANCH_NAME}"
+gh pr create --base main --head "${BRANCH_NAME}" --fill

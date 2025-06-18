b0928e6ee5087ffceb4798d555570939c6de07f7
Luiz Carvalho <lucarval@redhat.com>
2025-06-18 13:06:42 -0400

    Handle case in find-missing-deps
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

--- find-missing-deps.sh
+++ find-missing-deps.sh
@@ -7,4 +7,6 @@ set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-comm -13 <(ls -1 packages/) <(cat packages/*/requirements*.txt | grep '==' | cut -d= -f1 | sort -u)
+comm -13 \
+    <(ls -1 packages/) \
+    <(cat packages/*/requirements*.txt | grep '==' | cut -d= -f1 | sort -u | tr '[:upper:]' '[:lower:]')
